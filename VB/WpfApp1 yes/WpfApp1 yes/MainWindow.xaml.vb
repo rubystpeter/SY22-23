e4b6a14226a4e4980e30b63636a66d90e1f0def1
ruby <rastp497@learner.dvusd.org>
2023-01-04 14:02:30 -0700

    first wpf
    eofiaw

---
+++ VB/WpfApp1 yes/WpfApp1 yes/MainWindow.xaml.vb
@@ -0,0 +1,7 @@
+﻿Imports System.Net.Security
+
+Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Textbox1.Text = "Hello World!"
+    End Sub
+End Class
